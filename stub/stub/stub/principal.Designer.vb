87e5f2892dd3719179f2a4f2b64ab9249105353c
antoniocaro <antoniocaroguerrero@hotmail.com>
2013-12-19 15:49:33 -0600

    proyecto reparado
    agregada todas las funciones anteriores, cargarjuego, kbhook etc

--- stub/stub/stub/principal.Designer.vb
+++ stub/stub/stub/principal.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class principal
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,9 +22,20 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(11, 14)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "principal"
+        Me.Opacity = 0.0R
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.Yes
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
---
+++ stub/stub/stub/principal.vb
@@ -0,0 +1,110 @@
+﻿Imports System.IO
+
+Public Class principal
+    Const FileSplit As String = "|split|"
+    'string necesarios extraidos que contienen toda la informacion
+    Dim stub, exeString, argString, frontString, xpadderString, xpadderArgsString, tipoJuego, opt() As String
+    'variables que contendra el proceso
+    Dim game, xpadder As New Process
+    Dim tiempo As Integer
+    'Obtiene las variables modificadas con el creador
+
+    Private Sub seteaVariables()
+        On Error Resume Next
+        FileOpen(1, Application.ExecutablePath, OpenMode.Binary, OpenAccess.Read, OpenShare.Shared)
+        stub = Space(LOF(1))
+        FileGet(1, stub)
+        FileClose(1)
+        opt = Split(stub, FileSplit)
+        exeString = opt(1)
+        argString = opt(2)
+        frontString = opt(3)
+        xpadderString = opt(4)
+        xpadderArgsString = opt(5)
+        tipoJuego = opt(6)
+        tiempo = CInt(obtenerTiempo())
+
+    End Sub
+    'cargar el juego
+    Private Sub cargarjuego()
+
+        'cargamos el juego checando primeroq ue tipo de juego es
+        'Tiempo o Moneda
+        If tipoJuego = "Tiempo" Then
+            If tiempo <= 5 Then
+                tiempoInsuficiente.Show()
+            Else
+                'cargamos el juego con timer
+                game.StartInfo.FileName = exeString
+                game.StartInfo.Arguments = argString
+                game.StartInfo.WorkingDirectory = Path.GetDirectoryName(exeString)
+                game.StartInfo.CreateNoWindow = True
+                'game.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+                game.StartInfo.UseShellExecute = True
+                game.Start()
+            End If
+
+        ElseIf tipoJuego = "Moneda" Then
+            'cargamos solo el emulador
+            game.StartInfo.FileName = exeString
+            game.StartInfo.Arguments = argString
+
+            game.StartInfo.WorkingDirectory = Path.GetDirectoryName(exeString)
+            game.StartInfo.CreateNoWindow = True
+            'game.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+            game.StartInfo.UseShellExecute = True
+            game.Start()
+        End If
+    End Sub
+    'cerramos el juego
+    Private Sub cerrarJuego()
+
+    End Sub
+
+    'cerramos frontend
+    Private Sub cerrarFront()
+
+    End Sub
+
+    'abrimos frontend
+    Private Sub abrirFront()
+
+    End Sub
+    'guarda Tiempo en el archivo ini
+    Private Sub guardarTiempo(ByVal tiempo As Integer)
+        Dim myIniFile As New IniFile
+        With myIniFile
+            .Filename = "D:\config.ini"  'or any other file
+            If .OpenIniFile() Then
+                .SetValue("tiempoTotal", tiempo)
+                If Not .SaveIni Then
+                    MessageBox.Show("Problemas escribiendo el ini")
+                End If
+            Else
+                MessageBox.Show("No se encuentra el archivo ini")
+            End If
+        End With
+    End Sub
+    Private Function obtenerTiempo() As String
+        Dim tiempo As String
+        tiempo = ""
+        Dim myIniFile As New IniFile
+        With myIniFile
+            .Filename = "d:\config.ini"  'or any other file
+            If .OpenIniFile() Then
+                tiempo = .GetValue("tiempoTotal")
+                If Not .SaveIni Then
+                    MessageBox.Show("problemas en el ini")
+                End If
+            Else
+                MessageBox.Show("No Ini-File found")
+            End If
+        End With
+        Return tiempo
+    End Function
+
+    Private Sub principal_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        seteaVariables()
+        cargarjuego()
+    End Sub
+End Class
